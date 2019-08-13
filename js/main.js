aec458cc50d3238805ca4e226d20ceaf87c55044
LypkaB <lpk.bdn@gmail.com>
2019-08-13 21:03:17 +0300

    Added adaptive for section «header»

--- js/main.js
+++ js/main.js
@@ -1,26 +1,3 @@
-/* First variant */
-/*
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
-    var pos = document.getElementsByClassName("position");
-
-    var isOutOfRange = n > pos.length || n < 1;
-    if (isOutOfRange) return;
-
-    for (var i = 0; i < pos.length; i++) {
-        pos[i].className = pos[i].className.replace(" active", "");
-    }
-
-    pos[slideIndex-1].className += " active";
-}*/
-
-/* Second variant */
 var slideIndex = 1;
 var pos = document.getElementsByClassName("position");
 showSlides(slideIndex);
